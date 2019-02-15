d0ac0681b00d9f9a2897827860a612e9991e69fd
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-02-15 18:46:09 +0300

    add javascript structure

---
+++ script.js
@@ -0,0 +1,9 @@
+function(triangleTracker){
+  var sideQ = parseInt(document.getElementByid("sideQ").value);
+  var sideR = parseInt(document.getElementsByid("sideR").value);
+  var sideS = parseInt(document.getElementByid("sideS").value);
+  var side = [sideQ,sideR,sideS]
+
+
+
+}
