ef3131683ed94eda0daf1a6b800cf3c6eed8836c
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-02-18 10:44:32 +0300

    editing business logic code

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-function(triangleTracker){
-  var sideQ = parseInt(document.getElementByid("sideQ").value);
-  var sideR = parseInt(document.getElementsByid("sideR").value);
-  var sideS = parseInt(document.getElementByid("sideS").value);
-  var side = [sideQ,sideR,sideS]};
-  if(side[0]+side[1]<=side[2]||side[0]+side[2]<=side[1]||side[2+side[1]<=side[0]{
-    alert("This is not a triangle");
-  }else if(side[0]===side[1]&&side[0]===side[2]){
-    alert("This is an equilateral");
-  }else if(side[0]===side[1]||side[0]===side[2]||side[2]===side[1]){
-      alet("This is an isoscles triangle");
-  }else if(side{0}+side{1}>side{2}&&side{0}+side{2}>side{1}&&side[2]+side[1]>side[0]){
-    alert("This is a scalene");
+function triangleTracker(){
+  var sideQ = parseInt(document.getElementById("sideQ").value);
+  var sideR = parseInt(document.getElementsById("sideR").value);
+  var sideS = parseInt(document.getElementById("sideS").value);
+  var side = [sideQ,sideR,sideS];
+
+  if(side[0]+side[1]<=side[2]||side[0]+side[2]<=side[1]||side[2+side[1]<=side[0]){
+    alert("can not form a triangle from this sides ");
+      }
+     else if(side[0]===side[1]&&side[1]===side[2]&&side[2]===side[0]){
+      alert("This is an equilateral");
+    }
+    else if(side[0]+side[1]>side[2]||side[1]+side[2]>side[0]||side[0]+side[2]>side[1]){
+      alert("This is a scalene");
+    }
+    else if(side[0]===side[1]||side[0]===side[2]||side[1]===side[2]){
+      alert("This is an isosceles");
+    }
+  else{
+    alert("invalid entry");
+  }
+}
