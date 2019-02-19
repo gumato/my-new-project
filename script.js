7b8a213d849272347398d813a16fd4c8baafeb76
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-02-19 08:10:45 +0300

    created a space in between the comparison to debug

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function triangleTracker(){
   var sideR = parseInt(document.getElementById("sideR").value);
   var sideS = parseInt(document.getElementById("sideS").value);
   var side = [sideQ,sideR,sideS];
-  if (side[0]+side[1]>side[2]&&side[1]+side[2]>side[0]&&side[0]+side[2]>side[1]){
+  if (side[0]+side[1]>side[2] && side[1]+side[2]>side[0] && side[0]+side[2]>side[1]){
   else if(side[0]===side[1]&&side[0]===side[2]){
     alert("equilateral triangle");
   }
   else if(side[0]===side[1]||side[1]===side[2]||side[0]===side[2]){
     alert("isosceles triangle");
   }
-  else if(side[0]!==side[1]||side[1]!==side[2]||side[0]!==side[2]){
+  else if(side[0]!==side[1] || side[1]!==side[2] || side[0]!==side[2]){
     alert("scalene triangle");
   }
   else
