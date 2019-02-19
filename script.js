4f68340297533c44d5789301b1f57a41e623a3ef
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-02-19 07:44:41 +0300

    add an image to html file

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 function triangleTracker(){
   var sideQ = parseInt(document.getElementById("sideQ").value);
-  var sideR = parseInt(document.getElementsById("sideR").value);
+  var sideR = parseInt(document.getElementById("sideR").value);
   var sideS = parseInt(document.getElementById("sideS").value);
   var side = [sideQ,sideR,sideS];
-
-  if(side[0]+side[1]<=side[2]||side[0]+side[2]<=side[1]||side[2+side[1]<=side[0]){
-    alert("can not form a triangle from this sides ");
-      }
-     else if(side[0]===side[1]&&side[1]===side[2]&&side[2]===side[0]){
-      alert("This is an equilateral");
-    }
-    else if(side[0]+side[1]>side[2]||side[1]+side[2]>side[0]||side[0]+side[2]>side[1]){
-      alert("This is a scalene");
-    }
-    else if(side[0]===side[1]||side[0]===side[2]||side[1]===side[2]){
-      alert("This is an isosceles");
-    }
-  else{
-    alert("invalid entry");
+  if (side[0]+side[1]>side[2]&&side[1]+side[2]>side[0]&&side[0]+side[2]>side[1]){
+  else if(side[0]===side[1]&&side[0]===side[2]){
+    alert("equilateral triangle");
   }
-}
+  else if(side[0]===side[1]||side[1]===side[2]||side[0]===side[2]){
+    alert("isosceles triangle");
+  }
+  else if(side[0]!==side[1]||side[1]!==side[2]||side[0]!==side[2]){
+    alert("scalene triangle");
+  }
+  else
+  {
+    alert("not a triangle");
+  }
+  }
